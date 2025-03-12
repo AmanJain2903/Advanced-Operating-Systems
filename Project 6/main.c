6744ac7979ea07ef75a94ebc585e02c048ac65a5
Amarnath Kaushik <amarnathkaushik@Amarnaths-MacBook-Pro.local>
2025-03-11 23:53:16 -0700

    Added Parent Timestamp

--- Project 6/main.c
+++ Project 6/main.c
@@ -14,8 +14,8 @@
 #define OUTPUT_FILE "output.txt"
 
 FILE *output_file = NULL;
+struct timeval parent_start_tv;
 
-// Signal handler to flush and close the file gracefully
 void handle_signal(int signal) {
     if (output_file) {
         fflush(output_file);
@@ -25,103 +25,103 @@ void handle_signal(int signal) {
     exit(0);
 }
 
+void get_timestamp(char *buffer, size_t buffer_size) {
+    struct timeval current_tv;
+    gettimeofday(&current_tv, NULL);
+    double elapsed_time = (current_tv.tv_sec - parent_start_tv.tv_sec) +
+                         (current_tv.tv_usec - parent_start_tv.tv_usec) / 1000000.0;
+    int minutes = (int)(elapsed_time / 60);
+    int seconds = (int)elapsed_time % 60;
+    int milliseconds = (int)((elapsed_time - (int)elapsed_time) * 1000);
+    snprintf(buffer, buffer_size, "%d:%02d.%03d", minutes, seconds, milliseconds);
+}
+
 int main() {
     int fd[NUM_CHILDREN][2];
     pid_t pids[NUM_CHILDREN];
     fd_set read_fds;
     int max_fd = 0;
 
-    // Open the output file
     output_file = fopen(OUTPUT_FILE, "w");
     if (!output_file) {
         perror("Failed to open output file");
         exit(EXIT_FAILURE);
     }
 
-    // Register signal handlers for graceful termination
     signal(SIGINT, handle_signal);
     signal(SIGTERM, handle_signal);
+    gettimeofday(&parent_start_tv, NULL);
 
-    // Create pipes and fork child processes
     for (int i = 0; i < NUM_CHILDREN; i++) {
         if (pipe(fd[i]) == -1) {
             perror("pipe");
             exit(EXIT_FAILURE);
         }
-
         pids[i] = fork();
         if (pids[i] < 0) {
             perror("fork");
             exit(EXIT_FAILURE);
         }
-
         if (pids[i] == 0) {
-            // Child process
-            close(fd[i][READ_END]);
-            child_process(i, fd[i][WRITE_END]);
-            close(fd[i][WRITE_END]);
+            close(fd[i][0]);
+            child_process(i, fd[i][1]);
+            close(fd[i][1]);
             exit(0);
         } else {
-            // Parent process
-            close(fd[i][WRITE_END]);
-            if (fd[i][READ_END] > max_fd) {
-                max_fd = fd[i][READ_END];
+            close(fd[i][1]);
+            if (fd[i][0] > max_fd) {
+                max_fd = fd[i][0];
             }
         }
     }
 
-    // Parent process using select()
     while (1) {
         FD_ZERO(&read_fds);
         for (int i = 0; i < NUM_CHILDREN; i++) {
-            FD_SET(fd[i][READ_END], &read_fds);
+            FD_SET(fd[i][0], &read_fds);
         }
 
         int activity = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
-
         if (activity < 0) {
             perror("select");
             exit(EXIT_FAILURE);
         }
 
         for (int i = 0; i < NUM_CHILDREN; i++) {
-            if (FD_ISSET(fd[i][READ_END], &read_fds)) {
+            if (FD_ISSET(fd[i][0], &read_fds)) {
                 char buffer[BUFFER_SIZE];
-                ssize_t nread = read(fd[i][READ_END], buffer, BUFFER_SIZE - 1);
-
+                ssize_t nread = read(fd[i][0], buffer, BUFFER_SIZE - 1);
                 if (nread > 0) {
-                    buffer[nread] = '\0'; // Null-terminate the buffer
-                    fprintf(output_file, "%s", buffer);
+                    buffer[nread] = '\0';
+                    char parent_timestamp[20];
+                    get_timestamp(parent_timestamp, sizeof(parent_timestamp));
+                    char *line = strtok(buffer, "\n");
+                    while (line != NULL) {
+                        fprintf(output_file, "%s: Parent - %s\n", parent_timestamp, line);
+                        line = strtok(NULL, "\n");
+                    }
                     fflush(output_file);
                 } else if (nread == 0) {
-                    close(fd[i][READ_END]);
-                } else {
-                    perror("read");
-                    exit(EXIT_FAILURE);
+                    close(fd[i][0]);
                 }
             }
         }
 
-        // Check if all children have exited
         int all_children_exited = 1;
         for (int i = 0; i < NUM_CHILDREN; i++) {
-            if (FD_ISSET(fd[i][READ_END], &read_fds)) {
+            if (FD_ISSET(fd[i][0], &read_fds)) {
                 all_children_exited = 0;
                 break;
             }
         }
-
         if (all_children_exited) {
             break;
         }
     }
 
-    // Wait for all child processes to terminate
     for (int i = 0; i < NUM_CHILDREN; i++) {
         waitpid(pids[i], NULL, 0);
     }
-
-    // Close the output file
     fclose(output_file);
     printf("All child processes have terminated. Exiting parent process.\n");
     return 0;
